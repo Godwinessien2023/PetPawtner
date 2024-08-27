382b9cb38a5beb44ec15953fd5e255d922ddcf9c
Godwin Essien <kinggodwin4u@yahoo.com>
2024-08-27 23:48:26 +0100

    The following has been implemented. a Profile Model that links to registering users using a FK, a signup funtion using POST method that returns /signup, an index function that returns the home page, a login function using POST method that authenticates user and returns the app hme page of loged in user. The sign in function is still under construction though.

--- petpawtner/petpawtner/settings.py
+++ petpawtner/petpawtner/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add the core app to the installed apps
+    # Add the core app and every other app that will be created to the installed apps
     'core',
 ]
 
@@ -120,8 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
