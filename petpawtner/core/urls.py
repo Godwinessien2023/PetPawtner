b1b50014185cac28b699f6104439505713fd9033
Godwin Essien <kinggodwin4u@yahoo.com>
2024-09-01 22:52:59 +0100

    a pets and vet model has been added. a add-pets view has also been added to handle users who are pet owners to be able to add their pets. Also, settings view has been added to allow registers users select their roles either as pet owners or vets

--- petpawtner/core/urls.py
+++ petpawtner/core/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+
+"""
+add url patterns as will be used in the view.py
+"""
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
+    path('', views.index, name='index'),  # Home page
+    path('signup/', views.signup, name='signup'),  # User signup
+    path('settings/', views.settings, name='settings'),  # User settings
+    path('add_pets/', views.add_pets, name='add_pets'),  # Add pets page
+    path('signin/', views.signin, name='signin'),  # User signin
+    path('signout/', views.signout, name='signout'),  # User signout
 ]
